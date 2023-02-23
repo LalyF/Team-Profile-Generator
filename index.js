6f64dc96b781c550af74a6e33603c453d3aa1866
Laly <lalymanaf2015@gmail.com>
2023-02-23 23:55:08 +0000

    updated Manager.js

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ startProgram();
 async function startProgram(){
    let engineer= new Engineer("Andrew",55,"jbhjfbgj@gh.com","hjh")
    let en=engineer.getRole();
-   
-
+   let manager=new Manager("laly",56,"jsj@hjgjh.com",8566552)
+let mn=manager.getRole();
 team.push(engineer);
+team.push(manager);
 // team.push(new Engineer("jhjhj",55,"hjjhj@gh.com","jhj"));
 // team.push(new Engineer("hbhhj",55,"bjhbj@gh.com","bjhh"));
 
 
 // team.push(role);
-console.log(team)
-console.log(en);
+
 let htmlDoc=render(team);
 
 
