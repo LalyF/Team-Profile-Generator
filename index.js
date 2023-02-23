2658921a5726f5c7e58f294527a243b49d9a07bc
Laly <lalymanaf2015@gmail.com>
2023-02-23 23:39:34 +0000

    written fake value push to see if it is working in index.js

--- index.js
+++ index.js
@@ -3,13 +3,35 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const Employee = require("./lib/Employee");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+let team=[];
+startProgram();
+async function startProgram(){
+   let engineer= new Engineer("Andrew",55,"jbhjfbgj@gh.com","hjh")
+   let en=engineer.getRole();
+   
 
+team.push(engineer);
+// team.push(new Engineer("jhjhj",55,"hjjhj@gh.com","jhj"));
+// team.push(new Engineer("hbhhj",55,"bjhbj@gh.com","bjhh"));
+
+
+// team.push(role);
+console.log(team)
+console.log(en);
+let htmlDoc=render(team);
+
+
+await fs.writeFile(outputPath,htmlDoc);
+
+
+}
